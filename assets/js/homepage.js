60fbf841fb5c883ad55a25aad44aabfb53d5b06b
Marrione Nguyen <mtiamsic@gmail.com>
2020-08-27 08:21:27 -0700

    display api data on page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -6,7 +6,15 @@ var repoSearchTerm = document.querySelector("#repo-search-term");
 var getFeaturedRepos = function(language) {
     var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
 
-    fetch(apiUrl);
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+            displayRepos(data.items, language);
+        }); 
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    });
 };
 
 
